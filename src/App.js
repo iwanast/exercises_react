d646775b6c38f9f2cbbe146889fdbf3f22892547
Iwana Städeli <78117193+iwanast@users.noreply.github.com>
2022-02-10 11:00:40 +0100

    Accordion

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Accordion from "./Accordion";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+export default function App() {
+const items = [ {title: "What is react?",
+                content: "React is a frontend JS library. it is used to show content to users and handle user events. Engineers use React to create reusable components."},
+                { title: "Why use React?",
+                  content: "React is a popular JS library among engineers."},
+                { title: "How do you use react?",
+                  content: "You use React by creating component."}
+                ];
+
+  return(
+    <div>
+      <Accordion items={items} />
     </div>
-  );
+  )
 }
-
-export default App;
